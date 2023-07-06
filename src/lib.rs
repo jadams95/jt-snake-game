f184e900a6b60e8f6de3f3c78651f26426d4f1b6
John Adams <adamsjt95@gmail.com>
2023-07-06 07:18:55 -0400

    updated to have world on the canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,48 @@
+
+
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+
+struct SnakeCell(usize);
+
+
+pub struct Snake {
+    body: Vec<SnakeCell>
+}
+
+
+impl Snake {
+    fn new (spawn_index: usize) -> Snake {
+        Snake {
+            body: vec!(SnakeCell(spawn_index))
+        }
+    }
+}
 
 #[wasm_bindgen]
-pub fn greet(name: &str){
-    println!("Hi there {}", name);
+pub struct World {
+    pub width: usize,
+    snake: Snake,
 }
+
+#[wasm_bindgen]
+impl World {
+    pub fn new () -> World { 
+        World { width: 8,
+        snake: Snake::new(10) }
+    }
+    pub fn width (&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+}
+
+
--- www/index.js
+++ www/index.js
@@ -1,33 +1,53 @@
-async function init(){
+import init, {World} from "jt-snake-game";
 
-    const memory = new WebAssembly.Memory({
-        initial: 1
-    });
-    const importObject = {
-        js: {
-            mem: memory
-        },
-        console: {
-            log: () => {
-                console.log("Just logging ---->\n");
-            },
-            error: () => {
-                console.log("Error -------> \n");
-            }
+
+
+// async function start(){
+//     const wasm = init();
+//     greet("Filip!");
+//     console.log("OK!")
+
+// }
+
+init().then(_ => {
+    const CELL_SIZE = 40;
+    const world = World.new();
+    const worldWidth = world.width();
+    console.log(world.width());
+    const canvas = document.getElementById("snake-canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.height = worldWidth * CELL_SIZE;
+    canvas.width = worldWidth * CELL_SIZE;
+
+    function drawWorld(){
+        ctx.beginPath();
+        
+        for(let x = 0; x < worldWidth + 1; x++){
+            ctx.moveTo(CELL_SIZE * x, 0);
+            ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
         }
+        
+        for(let y = 0; y < worldWidth + 1; y++){
+            ctx.moveTo(0, CELL_SIZE * y);
+            ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+        }
+        ctx.stroke();
     }
-    const resp = await fetch("sum.wasm");
-    const buffer = await resp.arrayBuffer();
-    // const byteArray = new Int8Array([0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01, 0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07, 0x07, 0x01, 0x03, 0x73, 0x75, 0x6d, 0x00, 0x00, 0x0a, 0x09, 0x01, 0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b, 0x00, 0x18, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x01, 0x06, 0x01, 0x00, 0x03, 0x73, 0x75, 0x6d, 0x02, 0x09, 0x01, 0x00, 0x02, 0x00, 0x01, 0x61, 0x01, 0x01, 0x62]);
-debugger
-await WebAssembly.instantiate(buffer, importObject);
-// const sumFunction = wasm.instance.exports.sum;
-debugger
-const uint8Array = new Uint8Array(memory.buffer, 0, 2);
-const hiText = new TextDecoder().decode(uint8Array);
-console.log(hiText);
-// debugger
-// const result = sumFunction(100, 1);
-// console.log(result);
-}
-init()
+    function drawSnake(){
+        const snakeIdx = world.snake_head_idx();
+        const col = snakeIdx % worldWidth;
+        const row = Math.floor(snakeIdx / worldWidth);
+        ctx.beginPath();
+        ctx.fillRect(
+            col * CELL_SIZE,
+            row * CELL_SIZE,
+            CELL_SIZE, 
+            CELL_SIZE
+        );
+        ctx.stroke();
+    }
+
+    console.log(world.snake_head_idx());
+    drawWorld();
+    drawSnake();
+});
